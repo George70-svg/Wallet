9cb80bcf5da0fd6c150b078fd3117db49c46e865
Anton <asiuraev@parseq.pro>
2023-04-23 18:13:25 +0800

    Change styles for deploy.

--- src/components/loginPage/loginPage.tsx
+++ src/components/loginPage/loginPage.tsx
@@ -5,13 +5,7 @@ import { LoginContainer } from '@components/loginPage/components/loginContainer'
 export function LoginPage() {
   return (
     <StyledLoginPage>
-      <div className='landscape'>
-        <h3>Sorry, please turn your device</h3>
-      </div>
-
-      <div className='portrait'>
-        <LoginContainer />
-      </div>
+      <LoginContainer />
     </StyledLoginPage>
   )
 }
